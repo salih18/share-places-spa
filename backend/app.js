3b06dad43cb0975ff201b84962273bbb34298753
salih <salihsert18@gmail.com>
2020-06-11 19:57:46 +0200

    heroku config

--- backend/app.js
+++ backend/app.js
@@ -52,6 +52,15 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({ message: error.message || 'An unKnown error occurred!' });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 // Connect the express server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
